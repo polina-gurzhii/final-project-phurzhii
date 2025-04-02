b2f89d9f972625cc433cce467856cf873715f73d
polina-gurzhii <polya.gyrzhiy@gmail.com>
2025-04-02 22:34:47 +0300

    tests are done

--- tests/test-1-3.spec.ts
+++ tests/test-1-3.spec.ts
@@ -12,7 +12,7 @@ test('Verify login with valid credentials', async ({ page }) => {
 
 test('Verify user can view product details', async ({ page }) => {
   await page.goto('/');
-  await page.locator('[data-test="product-01JQS85TMHWS2S3BKVGMESQ1QK"]').click();
+  await page.getByRole('heading', {name: 'Combination Pliers'}).click();
 
   await expect(page.url()).toContain('/product');
   await expect(page.locator('[data-test="product-name"]')).toContainText('Combination Pliers');
@@ -24,5 +24,25 @@ test('Verify user can view product details', async ({ page }) => {
 });
 
 test('Verify user can add product to cart', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('https://practicesoftwaretesting.com/');
+  await page.getByRole('heading', {name: 'Slip Joint Pliers'}).click();
+  await expect(page.url()).toContain('/product');
+  await expect(page.locator('[data-test="product-name"]')).toContainText('Slip Joint Pliers');
+  await expect (page.locator('[data-test="unit-price"]')).toContainText('9.17');
+
+  await page.locator('[data-test="add-to-cart"]').click();
+  const cartAlert = page.getByRole('alert');
+  await expect (cartAlert).toBeVisible();
+  await expect(cartAlert).toHaveText('Product added to shopping cart.');
+  await expect(cartAlert).toBeHidden({ timeout: 8_000 });
+  await expect (page.locator('[data-test="cart-quantity"]')).toHaveText('1');
+  
+
+  await page.locator('[data-test="nav-cart"]').click();
+  await expect(page).toHaveURL('/checkout');
+  await expect (page.locator('[data-test="product-title"]')).toHaveCount(1);
+  await expect (page.locator('[data-test="product-title"]')).toHaveText('Slip Joint Pliers');
+  await expect (page.locator('[data-test="proceed-1"]')).toBeVisible();
+  
 });
+
