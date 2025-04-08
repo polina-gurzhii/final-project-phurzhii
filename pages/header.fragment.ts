363b14807cbe179b0cdd20ad098f25763e956b4b
polina-gurzhii <polya.gyrzhiy@gmail.com>
2025-04-08 16:47:44 +0300

    1st commit

---
+++ pages/header.fragment.ts
@@ -0,0 +1,5 @@
+export class HeaderF {
+    constructor(readonly page) {
+
+    }
+};
---
+++ pages/login.page.ts
@@ -0,0 +1,23 @@
+import { Locator, Page } from '@playwright/test';
+import { HeaderF } from './header.fragment';
+
+export class LoginPage {
+    page: Page;
+    emailLocator: Locator;
+    password: Locator;
+    submitButton: Locator;
+    header: HeaderF;
+
+    constructor(page: Page){
+        this.page = page;
+        this.emailLocator = this.page.locator('[data-test="email"]');
+        this.password = this.page.locator('[data-test="password"]');
+        this.submitButton = this.page.locator('[data-test="login-submit"]');
+        this.header = new HeaderF(page);
+    }
+async login(email: string, password: string): Promise<void> {
+    await this.emailLocator.fill(process.env.USER_EMAIL);
+    await this.password.fill(process.env.USER_PASSWORD!);
+    await this.submitButton.click();
+}
+};
