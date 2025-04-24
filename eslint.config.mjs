9549eb060e09aac27c1f02174aa455deb4d5f2c9
polina-gurzhii <polya.gyrzhiy@gmail.com>
2025-04-24 17:05:36 +0300

    1st commit

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import playwright from 'eslint-plugin-playwright';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+        parserOptions: {
+          projectService: {
+            allowDefaultProject: ['eslint.config.mjs'],
+          },
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
+    },
+    {
+        rules: {
+          '@typescript-eslint/no-floating-promises': 'error',
+        },
+      },
+      {
+        ...playwright.configs['flat/recommended'],
+        files: ['tests/**'],
+        rules: {
+          ...playwright.configs['flat/recommended'].rules,
+          'playwright/expect-expect': 'off',
+          'playwright/no-skipped-test': 'off',
+          'playwright/no-commented-out-tests': 'warn',
+        },
+      }
+    );
